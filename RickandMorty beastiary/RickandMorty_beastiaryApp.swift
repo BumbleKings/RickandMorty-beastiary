85cbc02390914757ee83876e034c557f69debdcf
Wyatt, Leteef NZ/IBC-ZGD <@schaeffler.com>
2026-09-10 12:08:39 -0400

    Initial Commit

---
+++ RickandMorty beastiary/RickandMorty_beastiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickandMorty_beastiaryApp.swift
+//  RickandMorty beastiary
+//
+//  Created by Wyatt, Leteef NZ/IBC-ZGD on 9/10/26.
+//
+
+import SwiftUI
+
+@main
+struct RickandMorty_beastiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RickandMorty beastiaryTests/RickandMorty_beastiaryTests.swift
@@ -0,0 +1,19 @@
+//
+//  RickandMorty_beastiaryTests.swift
+//  RickandMorty beastiaryTests
+//
+//  Created by Wyatt, Leteef NZ/IBC-ZGD on 9/10/26.
+//
+
+import Testing
+@testable import RickandMorty_beastiary
+
+struct RickandMorty_beastiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
